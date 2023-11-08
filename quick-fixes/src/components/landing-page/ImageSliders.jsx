8aa408c3d697c7dc205b3307dd38cffecf3dd518
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-11-08 14:16:21 -0500

    create slider component

---
+++ quick-fixes/src/components/landing-page/ImageSliders.jsx
@@ -0,0 +1,17 @@
+function ImageSlider ({props}) {
+
+    return (
+        <div className="wrapper">
+            <img src="https://assets.newatlas.com/dims4/default/3d94d96/2147483647/strip/true/crop/4461x2974+0+0/resize/1200x800!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2F38%2Fcc%2Fcb4aa02c4b0ab31f1e5a126d8c9b%2F01-5563-gawthorneshut-caarch-ambercreative.jpg" alt="Tiny house" />
+            {/*<img src="https://i.pinimg.com/736x/69/b4/33/69b4334df513513c68711724929584b3.jpg" alt="Tiny house" />
+            <img src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/home-improvement/wp-content/uploads/2023/04/tiny-home-modern-e1681126651196.jpeg" alt="Tiny house" />
+            <img src="https://kwpproducts.com/wp-content/uploads/2019/08/Tiny-home-blog-pic.jpg" alt="Tiny house" />
+            <img src="https://s1.cdn.autoevolution.com/images/news/this-not-so-tiny-home-has-an-outdoor-indoor-flow-and-even-a-downstairs-bedroom-212829_1.jpg" alt="Tiny house" />
+            <img src="https://a.storyblok.com/f/157322/df1a8c54f9/prefab-tiny-homes-for-sale-online.jpg" alt="Tiny house"/>
+            <img src="https://www.bostonmagazine.com/wp-content/uploads/sites/2/2020/01/rockport-4.jpg" alt="Tiny house" />*/}
+        </div>
+    )
+    
+}
+
+export default ImageSlider
