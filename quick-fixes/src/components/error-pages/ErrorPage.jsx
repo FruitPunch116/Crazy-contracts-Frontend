896fe511191375efc4f5a20d012052274da1d177
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-11-08 14:17:07 -0500

    create errr page

---
+++ quick-fixes/src/components/error-pages/ErrorPage.jsx
@@ -0,0 +1,8 @@
+export default function ErrorPage ({ props }) {
+  return (
+    <div>
+      <img src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1698883200&semt=ais" alt="404 error" t/>
+      <h3>Oh no! Apparently the site you are looking for is now available at this moment.</h3>
+    </div>
+  )
+}
