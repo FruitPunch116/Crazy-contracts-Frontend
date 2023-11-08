c47abbedfe422e53aa55b43fad204aa459263830
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-11-08 13:42:43 -0500

    sign up container

---
+++ quick-fixes/src/components/sign-up/SignUp.jsx
@@ -0,0 +1,12 @@
+import RoleSignUp from "./RoleSignUp.jsx"
+
+function SignUp ({props}) {
+
+    return (
+        <div className="sign-up-container">
+            <RoleSignUp />
+        </div>
+    )
+}
+
+export default SignUp
